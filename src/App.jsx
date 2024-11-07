dda55b65ff2b222720eadcdeaa8bd87bbe55ca67
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-07 00:54:44 -0300

    Sexto Commit: Desenvolvimento e instalação das dependências de rotas (react-router-dom)

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,30 @@
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home/Home"
+
 
 function App() {
 
   return (
 
     <>
-      <Header />
 
-      <h1 className="text-h1 font-montserrat text-second">
-        Olá Mundo!
-      </h1>
-      <h2 className="text-h2 font-montserrat">Olá Mundo!</h2>
-      <h3 className="text-h3 font-montserrat">Olá Mundo!</h3>
-      <h4 className="text-h4 font-montserrat">Olá Mundo!</h4>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-     
-      <h1 className="text-h1 font-montserrat text-second">
-        Olá Mundo!
-      </h1>
-      <h2 className="text-h2 font-montserrat">Olá Mundo!</h2>
-      <h3 className="text-h3 font-montserrat">Olá Mundo!</h3>
-      <h4 className="text-h4 font-montserrat">Olá Mundo!</h4>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
+      <Router>
 
-      <h1 className="text-h1 font-montserrat text-second">
-        Olá Mundo!
-      </h1>
-      <h2 className="text-h2 font-montserrat">Olá Mundo!</h2>
-      <h3 className="text-h3 font-montserrat">Olá Mundo!</h3>
-      <h4 className="text-h4 font-montserrat">Olá Mundo!</h4>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
+        <Header />
 
-      <h1 className="text-h1 font-montserrat text-second">
-        Olá Mundo!
-      </h1>
-      <h2 className="text-h2 font-montserrat">Olá Mundo!</h2>
-      <h3 className="text-h3 font-montserrat">Olá Mundo!</h3>
-      <h4 className="text-h4 font-montserrat">Olá Mundo!</h4>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
+        <Routes>
 
-      <h1 className="text-h1 font-montserrat text-second">
-        Olá Mundo!
-      </h1>
-      <h2 className="text-h2 font-montserrat">Olá Mundo!</h2>
-      <h3 className="text-h3 font-montserrat">Olá Mundo!</h3>
-      <h4 className="text-h4 font-montserrat">Olá Mundo!</h4>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
-      <h5 className="text-h5 font-montserrat">Olá Mundo!</h5>
+          <Route path="/" element={<Home />} />
 
-      <Footer />
+        </Routes>
+
+        <Footer />
+
+      </Router>
+
     </>
   )
 
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+            <h1>olá mundo</h1>
+        </>
+    )
+}
+
+export default HeroSection
