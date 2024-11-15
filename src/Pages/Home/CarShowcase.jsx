85518e9722a3b1389119ff852cd59b480afa7ee4
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-15 12:40:47 -0300

    Componente Home criado (prox att quando houver o Back-End)

--- src/Pages/Home/CarShowcase.jsx
+++ src/Pages/Home/CarShowcase.jsx
@@ -1,8 +1,10 @@
 import React, { forwardRef } from "react"
+import CarouselMarca from "../../components/CarouselMarca"
 import Button from "../../components/Button"
 import Cards from "../../components/Cards"
 
-const CarShowcase = forwardRef ((props, ref) => {
+
+const CarShowcase = forwardRef((props, ref) => {
 
     return (
         <section ref={ref} className="bg-black flex flex-col items-center justify-center">
@@ -10,8 +12,8 @@ const CarShowcase = forwardRef ((props, ref) => {
             <div className="flex flex-col md:flex-row items-center justify-center bg-second rounded-3xl px-10 md:px-16 py-4 md:py-6  my-10 gap-4 md:gap-16">
 
                 <span className="flex flex-row gap-1 md:gap-4 bg-gray-500 rounded-2xl pr-2 md:pr-4 ">
-                    <input type="search" name="searchBar" id="searchBar" placeholder="Pesquisar" className="rounded-2xl bg-white text-h5 md:text-h3 py-2 md:py-4 pl-4 md:pl-8"/>
-                    <button><img src="../src/assents/FiltroImg.png" alt="Filtro" /></button> 
+                    <input type="search" name="searchBar" id="searchBar" placeholder="Pesquisar" className="rounded-2xl bg-white text-h5 md:text-h3 py-2 md:py-4 pl-4 md:pl-8" />
+                    <button><img src="../src/assents/FiltroImg.png" alt="Filtro" /></button>
                 </span>
 
                 <Button className="text-h5 md:text-h3 px-8" color="bg-primary text-second hover:text-primary">
@@ -24,7 +26,7 @@ const CarShowcase = forwardRef ((props, ref) => {
                 <h1 className="text-h3 md:text-h1 text-primary">CARROS EM OFERTAS</h1>
             </div>
 
-            <div className="flex flex-col md:flex-row justify-center flex-wrap gap-4 py-4 md:py-8 md:m-12 rounded-md md:rounded-2xl bg-white">
+            <div className="flex flex-col md:flex-row justify-center flex-wrap gap-4 py-4 md:py-8 my-8 md:m-12 rounded-md md:rounded-2xl bg-white">
 
                 <Cards />
                 <Cards />
@@ -33,11 +35,18 @@ const CarShowcase = forwardRef ((props, ref) => {
                 <Cards />
                 <Cards />
 
-                <div>
-                    
-                </div>
+                {/* <div> Criar Paginação no Back-end </div> */}
 
             </div>
+
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-h3 md:text-h1 text-primary">LOJAS ASSOCIADAS</h1>
+
+                <div className="flex flex-row justify-center my-4">
+                    <CarouselMarca />
+                </div>
+            </div>
+            
 
         </section>
     )
---
+++ src/components/CarouselMarca.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react"
+
+const CarouselMarca = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const images = [
+    "../src/assents/BmwLogo.png",
+    "../src/assents/BmwLogo.png",
+    "../src/assents/BmwLogo.png",
+    "../src/assents/BmwLogo.png",
+    "../src/assents/BmwLogo.png",
+  ]
+
+  const visibleSlides = 3
+
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) =>
+      (prevIndex + 1) % (images.length - (visibleSlides - 1))
+    )
+  }
+
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      (prevIndex - 1 + images.length - (visibleSlides - 1)) %
+      (images.length - (visibleSlides - 1))
+    )
+  }
+
+  return (
+    <div className="relative w-full overflow-hidden">
+
+      <div className="flex transition-transform duration-700 ease-in-out"
+        style={{
+          transform: `translateX(-${(activeIndex * 100) / visibleSlides}%)`,
+        }}
+      >
+        {images.map((src, index) => (
+          <div key={index} className=" flex flex-shrink-0 justify-center w-1/3 px-2">
+            <img rounded-3xl
+              src={src}
+              alt={`Slide ${index + 1}`}
+              className="w-full md:w-auto object-contain rounded-full "
+            />
+          </div>
+        ))}
+      </div>
+
+      <button
+        type="button"
+        className="absolute top-1/2 left-4 z-30 -translate-y-1/2 bg-white/30 rounded-full p-2 hover:bg-white focus:outline-none focus:ring-2 focus:ring-white"
+        onClick={prevSlide}
+      >
+        <svg
+          className="w-6 h-6 text-black"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+      </button>
+      <button
+        type="button"
+        className="absolute top-1/2 right-4 z-30 -translate-y-1/2 bg-white/30 rounded-full p-2 hover:bg-white focus:outline-none focus:ring-2 focus:ring-white"
+        onClick={nextSlide}
+      >
+        <svg
+          className="w-6 h-6 text-black"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default CarouselMarca
