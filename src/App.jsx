e2f5e35ed08db4495ca6cc89e5c420f933d5203c
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-17 14:50:55 -0300

    add src/pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./pages/home"
 import About from "./pages/about"
 import CarDetails from "./pages/carDetails"
