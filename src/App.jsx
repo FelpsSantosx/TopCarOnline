23cb8edcdff07d9b1b327c59de688a7f0a4a65c8
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-18 22:42:47 -0300

    fotter posição 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
+import Home from "./pages/home"
 import Footer from "./components/Footer"
-import CarDetails from "./pages/carDetails/index.jsx"
-import About from "./pages/about/index.jsx"
-import Home from "./pages/home/index.jsx"
+import About from "./pages/about"
+import CarDetails from "./pages/carDetails"
 
 function App() {
 
