af98f4a6cf0c51673721072fbe41322da828f629
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-18 22:44:33 -0300

    ordenação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
-import Home from "./pages/home"
 import Footer from "./components/Footer"
 import About from "./pages/about"
 import CarDetails from "./pages/carDetails"
+import Home from "./pages/home"
 
 function App() {
 
