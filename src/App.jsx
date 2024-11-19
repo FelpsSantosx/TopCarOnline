3bf72ed189a3ac0a39949ee81f2f90503fce766c
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-18 23:26:23 -0300

    Commit Teste

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+// import About from "./pages/about"
+// import CarDetails from "./pages/carDetails"
 import Home from "./pages/home"
-import About from "./pages/about"
-import CarDetails from "./pages/carDetails"
 
 function App() {
 
@@ -14,8 +14,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/carDetails" element={<CarDetails />}/>
+          {/* <Route path="/about" element={<About />}/> */}
+          {/* <Route path="/carDetails" element={<CarDetails />}/> */}
         </Routes>
         <Footer />
       </Router>
