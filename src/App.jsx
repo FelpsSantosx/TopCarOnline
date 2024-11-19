f083134f22cf0576e41ab7216152e0dce7ec0e63
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-18 22:30:23 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import About from "./pages/about"
-import CarDetails from "./pages/carDetails/index.jsx"
 import Home from "./pages/home/index.jsx"
+import About from "./pages/about/index.jsx"
+import CarDetails from "./pages/carDetails/index.jsx"
 
 function App() {
 
