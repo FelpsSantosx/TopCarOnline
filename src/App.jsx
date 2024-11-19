af6e1d4333a630cd35639e2630c9623603e071cf
FelpsSantosx <lipesouzasanto@gmail.com>
2024-11-18 22:32:09 -0300

    ordem dos imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Home from "./pages/home/index.jsx"
-import About from "./pages/about/index.jsx"
 import CarDetails from "./pages/carDetails/index.jsx"
+import About from "./pages/about/index.jsx"
+import Home from "./pages/home/index.jsx"
 
 function App() {
 
