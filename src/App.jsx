492e123a3e419c8096acd51ae723bebd024f2ce1
FelpsSantosx <lipesouzasanto@gmail.com>
2024-12-01 14:00:35 -0300

    teste teste

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Router, Route, Routes } from "react-router-dom"
 import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Home from "./pages/home/Home"
+import Home from "./pages/home"
 import CarDetails from "./pages/carDetails"
 import About from "./pages/about"
 import SignIn from "./pages/signIn"
@@ -13,16 +13,16 @@ function App() {
 
     <>
 
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/carDetails" element={<CarDetails />} />
-          <Route path="/signIn" element={<SignIn />} />
-        </Routes>
-        <Footer />
-      </Router>
+
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/carDetails" element={<CarDetails />} />
+        <Route path="/signIn" element={<SignIn />} />
+      </Routes>
+      <Footer />
+
 
 
     </>
